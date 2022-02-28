e78d8309642344cedbc9700e2f62e67327b435f6
Sara-Barbi <barbisara02@gmail.com>
2022-02-28 17:53:01 +0100

    creazione cartelle

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . './Classi/Sala.php';
+require_once __DIR__ . './Classi/SalaImmersiva.php';
+require_once __DIR__ . './Classi/Spettacolo.php';
+require_once __DIR__ . './Classi/Film.php';
+
+$sale = [
+
+];
+
+$saleImmersive = [
+
+];
+
+$spettacoli = [
+
+];
+
+$opere = [
+
+];
+
